907ba654c39c02b5df417d72aba02e6d2100030c
freeloop <freeloop@gmail.com>
2022-12-12 21:49:55 +0800

    统一异常处理

---
+++ gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnum.java
@@ -0,0 +1,7 @@
+package com.atguigu.common.exception;/**
+ * BizCodeEnum
+ * 
+ * @author fj
+ * @date 2022/12/12 21:45
+ */public enum BizCodeEnum {
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/exception/GulimallExceptionControllerAdvice.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.product.exception;/**
+ * GulimallExceptionControllerAdvice
+ * 
+ * @author fj
+ * @date 2022/12/12 21:31
+ */public class GulimallExceptionControllerAdvice {
+}
