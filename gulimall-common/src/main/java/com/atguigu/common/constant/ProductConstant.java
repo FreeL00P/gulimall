dd154f6dfd0b37accf4e5392d179a032d58a2d15
freeloop <freeloop@gmail.com>
2022-12-14 21:49:15 +0800

    属性分组关联

---
+++ gulimall-common/src/main/java/com/atguigu/common/constant/ProductConstant.java
@@ -0,0 +1,10 @@
+package com.atguigu.common.constant;
+
+/**
+ * ProductConstand
+ *
+ * @author fj
+ * @date 2022/12/14 15:24
+ */
+public class ProductConstant {
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/AttrGroupRelationVo.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.product.vo;/**
+ * AttrGroupRelationVo
+ * 
+ * @author fj
+ * @date 2022/12/14 20:00
+ */public class AttrGroupRelationVo {
+}
