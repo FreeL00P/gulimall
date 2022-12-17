86f3b98bdca06f3485fb97ce0c18a76a40ef37b7
freeloop <freeloop@gmail.com>
2022-12-17 18:43:13 +0800

    商品发布

---
+++ gulimall-common/src/main/java/com/atguigu/common/to/SpuBoundsTo.java
@@ -0,0 +1,7 @@
+package com.atguigu.common.to;/**
+ * SpuBoundsTo
+ * 
+ * @author fj
+ * @date 2022/12/17 14:00
+ */public class SpuBoundsTo {
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/feign/CouponFeignService.java
@@ -0,0 +1,10 @@
+package com.atguigu.gulimall.product.feign;
+
+/**
+ * SpuFeignService
+ *
+ * @author fj
+ * @date 2022/12/17 13:55
+ */
+public interface CouponFeignService {
+}
