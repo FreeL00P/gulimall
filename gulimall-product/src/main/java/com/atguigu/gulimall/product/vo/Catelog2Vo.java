2502045fc91e2a67d584fe7539e95c8721a18174
freeloop <freeloop@gmail.com>
2022-12-21 20:59:32 +0800

    渲染二级三级分类数据

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Catelog2Vo.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.product.vo;/**
+ * Catelog2Vo
+ * 
+ * @author fj
+ * @date 2022/12/21 20:11
+ */public class Catelog2Vo {
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.product.web;/**
+ * IndexController
+ * 
+ * @author fj
+ * @date 2022/12/21 19:42
+ */public class IndexController {
+}
