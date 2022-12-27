9d58434b747eddc58df50090e84973666801c43f
freeloop <freeloop@gmail.com>
2022-12-27 20:44:26 +0800

    init 认证服务

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/authserver/config/gulimallWebConfig.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.authserver.config;/**
+ * gulimallWebConfig
+ * 
+ * @author fj
+ * @date 2022/12/27 20:38
+ */public class gulimallWebConfig {
+}
---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/authserver/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.authserver.controller;/**
+ * LoginController
+ * 
+ * @author fj
+ * @date 2022/12/27 19:44
+ */public class LoginController {
+}
