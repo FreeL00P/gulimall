5dc1030d3ab15450bf954c26488d492dc1345034
freeloop <freeloop@gmail.com>
2022-12-31 10:01:08 +0800

    账号密码登陆功能

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/authserver/vo/UserLoginVo.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.authserver.vo;/**
+ * UserLoginVo
+ * 
+ * @author fj
+ * @date 2022/12/31 9:06
+ */public class UserLoginVo {
+}
---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/vo/MemberLoginVo.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.authserver.vo;
+
+import lombok.Data;
+
+/**
+ * UserLoginVo
+ *
+ * @author fj
+ * @date 2022/12/31 9:06
+ */
+@Data
+public class UserLoginVo {
+
+    private String loginacct;
+
+    private String password;
+
+}
