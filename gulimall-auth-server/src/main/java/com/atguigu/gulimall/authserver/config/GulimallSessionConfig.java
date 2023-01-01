3eef7c478e5e49ab2a4d3c2bd0a630e0a0744945
freeloop <freeloop@gmail.com>
2023-01-01 13:41:35 +0800

    SpringSession解决分布式session问题

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/authserver/config/GulimallSessionConfig.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.authserver.config;/**
+ * GulimallSessionConfig
+ * 
+ * @author fj
+ * @date 2023/1/1 10:15
+ */public class GulimallSessionConfig {
+}
---
+++ gulimall-common/src/main/java/com/atguigu/common/vo/MemberResponseVo.java
@@ -0,0 +1,7 @@
+package com.atguigu.common.vo;/**
+ * MemberResponseVo
+ * 
+ * @author fj
+ * @date 2023/1/1 9:09
+ */public class MemberResponseVo {
+}
