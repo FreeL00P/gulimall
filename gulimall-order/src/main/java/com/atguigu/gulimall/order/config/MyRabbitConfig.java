d330f0912ca7a38ecad7766073f68b962b978e8c
freeloop <freeloop@gmail.com>
2023-02-13 22:16:29 +0800

    整合RabbitMQ

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyRabbitConfig.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.order.config;/**
+ * MyRabbitConfig
+ * 
+ * @author fj
+ * @date 2023/2/13 21:24
+ */public class MyRabbitConfig {
+}
---
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/User.java
@@ -0,0 +1,7 @@
+package com.atguigu.gulimall.order;/**
+ * User
+ * 
+ * @author fj
+ * @date 2023/2/13 22:00
+ */public class User {
+}
